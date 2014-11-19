aca201011766e69bed97b30f492b8cc39513522a
alizarion <selim.bensenouci@gmail.com>
2014-11-19 20:26:07 +0100

    Update ng-cordova-oauth.js

--- ng-cordova-oauth.js
+++ ng-cordova-oauth.js
@@ -106,10 +106,10 @@
              * @param    array appScope
              * @return   promise
              */
-            google: function(clientId, appScope,accessType) {
+            google: function(clientId, appScope) {
                 var deferred = $q.defer();
                 if(window.cordova) {
-                    var browserRef = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token'+(accessType ? '&access_type=offline' : 'access_type=online'), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+                    var browserRef = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
                     browserRef.addEventListener("loadstart", function(event) {
                         if((event.url).indexOf("http://localhost/callback") === 0) {
                             var callbackResponse = (event.url).split("#")[1];
@@ -131,10 +131,11 @@
                 }
                 return deferred.promise;
             },
-            oauth2: function(clientId, appScope,authEndPoint,accessType) {
+            oauth2: function(clientId, appScope,authEndPoint) {
                 var deferred = $q.defer();
+                //TODO implement code flow
                 if(window.cordova) {
-                    var browserRef = window.open(authEndPoint+'?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token'+(accessType ? '&access_type=offline' : 'access_type=online'), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+                    var browserRef = window.open(authEndPoint+'?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
                     browserRef.addEventListener("loadstart", function(event) {
                         if((event.url).indexOf("http://localhost/callback") === 0) {
                             var callbackResponse = (event.url).split("#")[1];
